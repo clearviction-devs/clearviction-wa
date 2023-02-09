6a54161bd1717f6a29ffb95c144bfb85d00ea234
Robert Shepley <shepleysound@gmail.com>
2023-02-09 10:21:16 -0800

    Add prettier and husky. Handles formatting and pre-commit hooks, respectively.

--- pages/card-test.tsx
+++ pages/card-test.tsx
@@ -1,44 +1,44 @@
-import { Box } from '@mui/material';
+import { Box } from "@mui/material";
 
-import InfoCard from '../components/InfoCard';
-import SectionContainer from '../components/SectionContainer';
+import InfoCard from "../components/InfoCard";
+import SectionContainer from "../components/SectionContainer";
 
 export default function CardTestPage() {
   return (
     <>
       <SectionContainer>
         <InfoCard
-          title='Card'
-          body='Cards can be used to convey information and an optional call to action'
-          imgsrc='/illustrations/washington.svg'
-          maxWidth='50%'
-          ctaLink='#'
-          ctaText='Click me'
+          title="Card"
+          body="Cards can be used to convey information and an optional call to action"
+          imgsrc="/illustrations/washington.svg"
+          maxWidth="50%"
+          ctaLink="#"
+          ctaText="Click me"
         />
-        <Box display='flex' mt={4} gap={4}>
+        <Box display="flex" mt={4} gap={4}>
           <InfoCard
-            title='Card 1'
+            title="Card 1"
             body={`Cards also accept markdown strings.\n- item 1\n- item 2\n\n#####Hello Card`}
-            imgsrc='/illustrations/washington.svg'
-            maxWidth='100%'
+            imgsrc="/illustrations/washington.svg"
+            maxWidth="100%"
           />
           <InfoCard
-            title='Card 2'
+            title="Card 2"
             body={`Cards also accept markdown strings.\n* item 1\n* item 2\n\n#####Hello Card`}
-            imgsrc='/illustrations/washington.svg'
-            maxWidth='100%'
+            imgsrc="/illustrations/washington.svg"
+            maxWidth="100%"
           />
           <InfoCard
-            title='Card 3'
+            title="Card 3"
             body={`Cards also accept markdown strings.\n- item 1\n- item 2\n\n#####Hello Card`}
-            imgsrc='/illustrations/washington.svg'
-            maxWidth='100%'
+            imgsrc="/illustrations/washington.svg"
+            maxWidth="100%"
           />
           <InfoCard
-            title='Card 4'
+            title="Card 4"
             body={`Cards also accept markdown strings.\n- item 1\n- item 2\n\n#####Hello Card`}
-            imgsrc='/illustrations/washington.svg'
-            maxWidth='100%'
+            imgsrc="/illustrations/washington.svg"
+            maxWidth="100%"
           />
         </Box>
       </SectionContainer>
