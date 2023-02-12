7e27042e62882ce50f5e95703180bb516100c4f3
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-02-12 16:57:42 -0600

    add donation form

--- components/DonationForm.tsx
+++ components/DonationForm.tsx
@@ -1,32 +1,32 @@
-import { Box } from '@mui/material';
+import { Box } from "@mui/material";
 
 // making this a component incase we want to add the donation form anywhere else
 export default function DonationForm() {
   return (
     <Box
       sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: {sm: 'center', md: 'flex-end'},
-        position: { xs: 'relative' },
-        marginBottom: { xs: '155px', md: '-410px' },
-        marginTop: { xs: '0px', md: '-422px' },
-        marginRight: {sm: 'unset', md: '5%', lg: '15%' },
-        top: { xs: '0px', md: '-140px' },
+        display: "flex",
+        alignItems: "center",
+        justifyContent: { sm: "center", md: "flex-end" },
+        position: { xs: "relative" },
+        marginBottom: { xs: "155px", md: "-410px" },
+        marginTop: { xs: "0px", md: "-422px" },
+        marginRight: { sm: "unset", md: "5%", lg: "15%" },
+        top: { xs: "0px", md: "-140px" },
       }}
     >
       <iframe
-        title='Clearviction Donation Form'
-        src='https://donorbox.org/embed/clearviction-givingtuesday?hide_donation_meter=true'
-        name='donorbox'
+        title="Clearviction Donation Form"
+        src="https://donorbox.org/embed/clearviction-givingtuesday?hide_donation_meter=true"
+        name="donorbox"
         // seamless='seamless'
-        height='900'
-        width='100%'
+        height="900"
+        width="100%"
         style={{
-            border: 'none',
-            maxWidth: '425px',
-            minWidth: '250px',
-            maxHeight: 'none !important',
+          border: "none",
+          maxWidth: "425px",
+          minWidth: "250px",
+          maxHeight: "none !important",
         }}
       />
     </Box>
