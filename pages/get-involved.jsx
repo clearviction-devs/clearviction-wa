40bdcab35ab63512ea920f79d661495986c4ea36
nurselaine <Elaine.t.huynh@gmail.com>
2023-02-23 00:07:41 -0800

    created get-involved page file and content file. Added content for get-involved page to content file

---
+++ pages/get-involved.jsx
@@ -0,0 +1,14 @@
+import { Grid } from '@mui/material';
+
+import HeroBanner from "../components/HeroBanner";
+
+export default function GetInvolvedPage(){
+  return (
+    <>
+      <HeroBanner 
+        heading="Get Involved"
+        subheading="There are many ways to participate with the Clearviction team, and we appreciate all of them!"
+      />
+    </>
+  )
+}
