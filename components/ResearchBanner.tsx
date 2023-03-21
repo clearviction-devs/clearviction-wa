fea32204d6faf497b337ac61d800dc1cb3c26363
Eleni Papanicolas <elenipapanicolas@gmail.com>
2023-03-21 10:58:45 -0400

    remove alt text and put href in a variable

--- components/ResearchBanner.tsx
+++ components/ResearchBanner.tsx
@@ -10,6 +10,9 @@ const researchBannerStyles: SxProps = {
   py: 4,
 };
 
+const researchGoogleFormLink: string =
+  "https://docs.google.com/forms/d/1KXmPrwzHeE8_EEL88RFkjOFP4S1A52Ode1vV6SJijao/viewform?edit_requested=true";
+
 export default function ResearchBanner() {
   return (
     <Box sx={researchBannerStyles}>
@@ -22,7 +25,7 @@ export default function ResearchBanner() {
             <Button
               fullWidth
               variant="contained"
-              href="https://docs.google.com/forms/d/1KXmPrwzHeE8_EEL88RFkjOFP4S1A52Ode1vV6SJijao/viewform?edit_requested=true"
+              href={researchGoogleFormLink}
               sx={(theme) => ({
                 backgroundColor: theme.palette.primary.contrastText,
                 color: "black",
@@ -36,10 +39,7 @@ export default function ResearchBanner() {
             </Button>
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
-            <Image
-              src={researchImage}
-              alt="a cartoon of a person adding to a to do list with a life size crayon"
-            />
+            <Image src={researchImage} alt="" />
           </Grid>
         </Grid>
       </SectionContainer>
