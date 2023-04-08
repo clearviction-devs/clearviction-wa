708fe04c7bb0214d6ec7dcf997399cea6f16e581
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-04-08 17:01:41 -0500

    Switch out donation widget

--- components/DonationForm.tsx
+++ components/DonationForm.tsx
@@ -1,21 +1,16 @@
 import { Box } from "@mui/material";
+import Script from 'next/script';
 
-// making this a component incase we want to add the donation form anywhere else
 export default function DonationForm() {
   return (
-    <Box>
-      <iframe
-        src="https://donorbox.org/embed/clearviction"
-        name="donorbox"
-        height="900px"
-        width="100%"
-        style={{
-          maxWidth: "500px",
-          minWidth: "250px",
-          maxHeight: "none!important",
-          borderWidth: "0px",
-        }}
-      ></iframe>
+    <Box 
+      id='donation-widget'
+      sx={{ backgroundColor: '#fff', borderRadius: '10px', padding: '30px' }}
+    >
+      {/* embedded donation widget from give lively */}
+      <Script src='https://secure.givelively.org/widgets/simple_donation/clearviction.js?show_suggested_amount_buttons=true&show_in_honor_of=true&address_required=false&has_required_custom_question=null&suggested_donation_amounts[]=25&suggested_donation_amounts[]=50&suggested_donation_amounts[]=100&suggested_donation_amounts[]=250' />
+      <div id="give-lively-widget" className="gl-simple-donation-widget"></div>
+
     </Box>
   );
 }
