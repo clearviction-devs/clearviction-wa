1ae547bb2a8ac325e04995c7c1fda4fa53f11bfd
Shondriane <shondriane.wise@yahoo.com>
2023-05-18 15:31:46 -0400

    share buttons addded to gitInvolved component

--- pages/get-involved.jsx
+++ pages/get-involved.jsx
@@ -15,6 +15,7 @@ import HeroBanner from "../components/HeroBanner";
 import { PaperSection } from "../components/PaperSection";
 import SectionContainer from "../components/SectionContainer";
 import content from "../content/get-involved";
+import ShareButtons from "../components/ShareButtons"
 
 export default function GetInvolvedPage() {
   return (
@@ -161,8 +162,9 @@ function Partner() {
                 {content.partnerPage.text[1]}
               </Typography>
               <Typography sx={{ my: 3 }} variant="body1">
-                {content.partnerPage.text[2]}
+                {content.partnerPage.text[2] }
               </Typography>
+              <ShareButtons/>
             </Grid>
           </Grid>
         </PaperSection>
