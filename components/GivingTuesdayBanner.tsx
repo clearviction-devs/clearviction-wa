aff31b49f28acbfacc2c7c03063a563f7c8795fb
Shondriane <shondriane.wise@yahoo.com>
2023-06-07 04:22:09 -0400

    building out donation banner component

---
+++ components/GivingTuesdayBanner.tsx
@@ -0,0 +1,36 @@
+import { Box, Button, Link,ThemeProvider} from '@mui/material';
+import React, { useState } from 'react';
+import { IoIosArrowForward } from 'react-icons/io';
+import theme from '../styles/themes/theme'
+
+
+
+interface GivingTuesdayBannerProps{
+    onClose:()=>void
+}
+const GivingTuesdayBanner: React.FC<GivingTuesdayBannerProps> = ({ onClose }) => {
+    const [isBannerVisible, setIsBannerVisible] = useState(true);
+
+    const closeBanner=()=>{
+        setIsBannerVisible(false)
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+          <>
+<Box>
+    <Box>
+        <Link href="/donate" onClick={closeBanner}>
+        <IoIosArrowForward/>
+        </Link>
+    </Box>
+</Box>
+<Button>
+    X
+</Button>
+          </>
+    </ThemeProvider>
+  );
+};
+
+export default GivingTuesdayBanner;
