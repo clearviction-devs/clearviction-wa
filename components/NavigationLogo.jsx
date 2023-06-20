1322531aaf4949a484210214bf487cb6b9755607
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-06-20 18:13:05 -0500

    nav logo responsive fix

--- components/NavigationLogo.jsx
+++ components/NavigationLogo.jsx
@@ -1,17 +1,13 @@
 import { ButtonBase } from '@mui/material';
+import Image from 'next/image';
 import PropTypes from 'prop-types';
 import React from 'react';
 
 export default function NavigationLogo({ fullSize = false }) {
+  const imgSrc = fullSize ? '/cv_logo_small.svg' : '/cv_logo_inline.svg';
   return (
     <ButtonBase href="/" sx={{ py: fullSize ? 2 : 1, height: '100%' }}>
-      {fullSize ? (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img src="/cv_logo_inline.svg" height="100%" alt="Clearviction logo" />
-      ) : (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img src="/cv_logo_small.svg" height="100%" alt="Clearviction logo" />
-      )}
+      <Image src={imgSrc} height={43} width={273} alt="Clearviction logo" />
     </ButtonBase>
   );
 }
