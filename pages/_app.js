40c23caa5004577dfd5be2c6f0d2f8189de0ba80
Shondriane <shondriane.wise@yahoo.com>
2023-06-21 15:52:15 -0400

    added google tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,43 +1,38 @@
 import '../styles/global.css';
-import TagManager from 'react-gtm-module';
+
 import { CacheProvider } from '@emotion/react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+
 import Footer from '../components/layout/Footer.tsx';
 import Header from '../components/layout/Header';
 import theme from '../styles/themes/theme.tsx';
 import createEmotionCache from '../utils/createEmotionCache';
 
-
 const clientSideEmotionCache = createEmotionCache();
 const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
-
-
 
 function MyApp({
   Component,
   pageProps,
   emotionCache = clientSideEmotionCache,
-  
 
 }) {
-  
- 
-   useEffect(() => {
-     TagManager.initialize({ gtmId:gtmId });
-
- }, [])
+  useEffect(() => {
+    TagManager.initialize({ gtmId });
+  }, []);
   return (
     <CacheProvider value={emotionCache}>
-      <Head>  
+      <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Header />
-      
+
         <Component {...pageProps} />
         <Footer />
       </ThemeProvider>
