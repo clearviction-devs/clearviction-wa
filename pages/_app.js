f76887ea8de644be49e5cc35f6c7751a857e6c1e
Shondriane <shondriane.wise@yahoo.com>
2023-06-21 15:51:38 -0400

    added react google tag manager module to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,43 @@
 import '../styles/global.css';
-
+import TagManager from 'react-gtm-module';
 import { CacheProvider } from '@emotion/react';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
-import React from 'react';
-
+import React, {useEffect} from 'react';
 import Footer from '../components/layout/Footer.tsx';
 import Header from '../components/layout/Header';
 import theme from '../styles/themes/theme.tsx';
 import createEmotionCache from '../utils/createEmotionCache';
 
+
 const clientSideEmotionCache = createEmotionCache();
+const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+
+
 
 function MyApp({
   Component,
   pageProps,
   emotionCache = clientSideEmotionCache,
+  
+
 }) {
+  
+ 
+   useEffect(() => {
+     TagManager.initialize({ gtmId:gtmId });
+
+ }, [])
   return (
     <CacheProvider value={emotionCache}>
-      <Head>
+      <Head>  
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Header />
+      
         <Component {...pageProps} />
         <Footer />
       </ThemeProvider>
