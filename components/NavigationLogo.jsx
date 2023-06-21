17e2f7f702d6b501604c9d6296469d55105a9fae
mhyneman8 <59981297+mhyneman8@users.noreply.github.com>
2023-06-20 19:15:27 -0500

    logo fix

--- components/NavigationLogo.jsx
+++ components/NavigationLogo.jsx
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default function NavigationLogo({ fullSize = false }) {
+  // images are misnamed
   const imgSrc = fullSize ? '/cv_logo_small.svg' : '/cv_logo_inline.svg';
-  const imgWidth = fullSize ? 273 : 50;
+  const imgWidth = fullSize ? 50 : 273;
   return (
     <ButtonBase className="nav-logo" href="/" sx={{ py: fullSize ? 2 : 1, height: '100%' }}>
       <Image src={imgSrc} height={43} width={imgWidth} alt="Clearviction logo" />
