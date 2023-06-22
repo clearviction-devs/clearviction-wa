c062746ec579d81e98c24d6b7d899938259907c2
Eleni Papanicolas <elenipapanicolas@gmail.com>
2023-06-22 13:02:07 -0400

    Update nav logo with props interface

--- components/NavigationLogo.tsx
+++ components/NavigationLogo.tsx
@@ -1,9 +1,12 @@
 import { ButtonBase } from '@mui/material';
 import Image from 'next/image';
-import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function NavigationLogo({ fullSize = false }) {
+interface NavigationLogoProps {
+  fullSize?: boolean;
+}
+
+export default function NavigationLogo({ fullSize = false }: NavigationLogoProps) {
   // images are misnamed
   const imgSrc = fullSize ? '/cv_logo_small.svg' : '/cv_logo_inline.svg';
   const imgWidth = fullSize ? 50 : 273;
@@ -13,7 +16,3 @@ export default function NavigationLogo({ fullSize = false }) {
     </ButtonBase>
   );
 }
-
-NavigationLogo.propTypes = {
-  fullSize: PropTypes.bool,
-};
