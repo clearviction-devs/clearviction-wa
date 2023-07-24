bfc6a3c529ba7c54239db80e9b2b478cb7e44849
Eleni Papanicolas <elenipapanicolas@gmail.com>
2023-07-24 13:20:58 -0400

    update: portable text component to ts

--- utils/portableTextComponents.tsx
+++ utils/portableTextComponents.tsx
@@ -4,6 +4,6 @@ import React from 'react';
 // eslint-disable-next-line import/prefer-default-export
 export const portableTextComponent = {
   marks: {
-    link: ({ children, value }) => <Link href={value.href}>{children}</Link>,
+    link: ({ children, value }: any) => <Link href={value.href}>{children}</Link>,
   },
 };
