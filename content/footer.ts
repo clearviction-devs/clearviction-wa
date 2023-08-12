53481ea46dfaa41ebd6a3d5e7e738bb9d73bd796
Reza Mashayekhi <rezamashayekhi22@yahoo.com>
2023-08-12 16:19:23 -0700

    Add footer content

---
+++ content/footer.ts
@@ -0,0 +1,18 @@
+const footerContent = {
+  mission:
+        'Clearviction is reducing barriers faced by formerly incarcerated individuals by streamlining the process of vacating eligible convictions in Washington state.',
+  address: {
+    name:
+        'Clearviction c/o Seamus Brugh',
+    street:
+        '107 Spring St',
+    city:
+        'Seattle, WA 98104',
+  },
+  warning:
+        'The content on this website should not be treated as legal advice',
+  information:
+        'Clearviction is a registered 501(c)3 nonprofit organization, EIN#88-3187952.  All donations are tax deductible in full or in part. ',
+};
+
+export default footerContent;
