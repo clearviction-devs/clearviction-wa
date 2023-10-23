dabf98911175ff4bc4fd39ae41a7168ad00095b9
Shondriane <shondriane.wise@yahoo.com>
2023-10-23 12:43:38 -0700

    made Container focusable with contentRef

--- pages/calculator/[slug].tsx
+++ pages/calculator/[slug].tsx
@@ -1,14 +1,15 @@
-import {
-  Box,
-  Container,
-  Typography,
-} from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import CalcHeader from '../../components/calculator/misdemeanor/CalcHeader.tsx';
-import FinalPageLinksContainer, { ErrorReportContainer, ShareCalcContainer } from '../../components/calculator/misdemeanor/CTAContainers.tsx';
-import NotSurePopup, { ShareCalculatorPopup } from '../../components/calculator/misdemeanor/PopupContainers.tsx';
+import FinalPageLinksContainer, {
+  ErrorReportContainer,
+  ShareCalcContainer,
+} from '../../components/calculator/misdemeanor/CTAContainers.tsx';
+import NotSurePopup, {
+  ShareCalculatorPopup,
+} from '../../components/calculator/misdemeanor/PopupContainers.tsx';
 import QandAContainer from '../../components/calculator/misdemeanor/QandAContainer.tsx';
 import ResultsDownloadContainer from '../../components/calculator/misdemeanor/ResultsDownloadContainer.tsx';
 import externalLinks from '../../components/functional/ExternalLinks.tsx';
@@ -22,20 +23,30 @@ import {
   getCalculatorPagePaths,
 } from '../../utils/sanity.client.ts';
 
-export default function CalculatorSlugRoute({ page, calculatorConfig }: StaticCalcProps) {
+export default function CalculatorSlugRoute({
+  page,
+  calculatorConfig
+}: StaticCalcProps) {
   // all state and functions here are shared between multiple secondary components
   const [openNotSurePopup, setOpenNotSurePopup] = useState(false);
   const [openSharePopup, setOpenSharePopup] = useState(false);
   const [responseObject, setResponseObject] = useState({});
   const [showResults, setShowResults] = useState(false);
 
-  const calcFirstPageUrl = 'https://clearviction.org/calculator/head-initial-1-cont';
+  const calcFirstPageUrl =
+    'https://clearviction.org/calculator/head-initial-1-cont';
   const isFirstPage = () => page.slug === 'head-initial-1-cont';
+  const contentRef = useRef<HTMLDivElement>(null);
 
   const addToResponses = (answer: string) => {
     // delete object when start over
     if (page.slug === 'head-initial-1-cont') setResponseObject({});
-    if (answer !== 'Continue' && answer !== 'Next' && answer !== 'Start' && page.slug !== 'head-mis-3-cont') {
+    if (
+      answer !== 'Continue'
+      && answer !== 'Next'
+      && answer !== 'Start'
+      && page.slug !== 'head-mis-3-cont'
+    ) {
       setResponseObject({ ...responseObject, [page.slug]: answer });
     }
   };
@@ -45,7 +56,11 @@ export default function CalculatorSlugRoute({ page, calculatorConfig }: StaticCa
   };
 
   externalLinks();
-
+  useEffect(() => {
+    if (contentRef.current) {
+      contentRef.current.focus();
+    }
+  }, [page]);
   return (
     <>
       <IndividualPageHead
@@ -56,6 +71,7 @@ export default function CalculatorSlugRoute({ page, calculatorConfig }: StaticCa
       <CalcHeader page={page} isFirstPage={isFirstPage} />
 
       <Container
+        ref={contentRef}
         maxWidth="md"
         sx={{
           display: 'flex',
@@ -64,8 +80,8 @@ export default function CalculatorSlugRoute({ page, calculatorConfig }: StaticCa
           justifyContent: 'center',
         }}
         id="calculator-container-outer"
+        tabIndex="-1"
       >
-
         <QandAContainer
           page={page}
           calculatorConfig={calculatorConfig}
@@ -73,42 +89,42 @@ export default function CalculatorSlugRoute({ page, calculatorConfig }: StaticCa
           setOpenNotSurePopup={setOpenNotSurePopup}
         />
 
-        {
-          (page.isFinalPage) && (
-            <>
-              <FinalPageLinksContainer
-                page={page}
-                calculatorConfig={calculatorConfig}
-                setOpenSharePopup={setOpenSharePopup}
-                calcFirstPageUrl={calcFirstPageUrl}
-              />
-              <Box maxWidth="60ch" textAlign="center" id="legal-disclaimer-container">
-                <Typography variant="caption" sx={{ fontWeight: 'light' }}>
-                  {calculatorConfig.legalDisclaimer}
-                </Typography>
-              </Box>
-            </>
-          )
-        }
+        {page.isFinalPage && (
+          <>
+            <FinalPageLinksContainer
+              page={page}
+              calculatorConfig={calculatorConfig}
+              setOpenSharePopup={setOpenSharePopup}
+              calcFirstPageUrl={calcFirstPageUrl}
+            />
+            <Box
+              maxWidth="60ch"
+              textAlign="center"
+              id="legal-disclaimer-container"
+            >
+              <Typography variant="caption" sx={{ fontWeight: 'light' }}>
+                {calculatorConfig.legalDisclaimer}
+              </Typography>
+            </Box>
+          </>
+        )}
 
-        {
-          (page.isFinalPage && page.isEligible) && (
-            <>
-              <ResultsDownloadContainer
-                handleCloseResults={handleCloseResults}
-                setShowResults={setShowResults}
-              />
-              <MailchimpForm />
-            </>
-          )
-        }
+        {page.isFinalPage && page.isEligible && (
+          <>
+            <ResultsDownloadContainer
+              handleCloseResults={handleCloseResults}
+              setShowResults={setShowResults}
+            />
+            <MailchimpForm />
+          </>
+        )}
 
-        {
-          (page.isEligible && showResults) && (
-            <Results responseObject={responseObject} handleCloseResults={handleCloseResults} />
-          )
-        }
-
+        {page.isEligible && showResults && (
+          <Results
+            responseObject={responseObject}
+            handleCloseResults={handleCloseResults}
+          />
+        )}
       </Container>
 
       <NotSurePopup
@@ -117,55 +133,54 @@ export default function CalculatorSlugRoute({ page, calculatorConfig }: StaticCa
         setOpenNotSurePopup={setOpenNotSurePopup}
       />
 
-      <ShareCalculatorPopup openSharePopup={openSharePopup} setOpenSharePopup={setOpenSharePopup} />
+      <ShareCalculatorPopup
+        openSharePopup={openSharePopup}
+        setOpenSharePopup={setOpenSharePopup}
+      />
 
       <Box sx={{ mb: '1.875rem' }}>
-
-        {
-          isFirstPage() && (
-            <ShareCalcContainer
-              setOpenSharePopup={setOpenSharePopup}
-              calcFirstPageUrl={calcFirstPageUrl}
-              justify
-            />
-          )
-          }
+        {isFirstPage() && (
+          <ShareCalcContainer
+            setOpenSharePopup={setOpenSharePopup}
+            calcFirstPageUrl={calcFirstPageUrl}
+            justify
+          />
+        )}
 
         <ErrorReportContainer calculatorConfig={calculatorConfig} />
       </Box>
-
     </>
-  );
+  )
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { params = {} } = ctx;
-  const slug = params.slug as string || '';
+  const slug = (params.slug as string) || '';
 
   const [page, calculatorConfig] = await Promise.all([
     getCalculatorPageBySlug({ slug }),
-    getCalculatorConfig(),
+    getCalculatorConfig()
   ]);
 
   if (!page) {
     return {
-      notFound: true,
-    };
+      notFound: true
+    }
   }
 
   return {
     props: {
       page,
-      calculatorConfig,
-    },
-  };
-};
+      calculatorConfig
+    }
+  }
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = await getCalculatorPagePaths();
 
   return {
     paths: paths?.map((slug) => `/calculator/${slug}`) || [],
-    fallback: false,
-  };
-};
+    fallback: false
+  }
+}
