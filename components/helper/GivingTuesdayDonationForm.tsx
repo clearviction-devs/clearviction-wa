a0cb13da69ef005a59051b932ea78a90e534f5d1
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-11-26 19:55:48 -0300

    fix: update donation form

--- components/helper/GivingTuesdayDonationForm.tsx
+++ components/helper/GivingTuesdayDonationForm.tsx
@@ -9,7 +9,7 @@ export default function GivingTuesdayDonationForm() {
       sx={{ backgroundColor: '#fff', borderRadius: '10px', padding: '30px' }}
     >
       {/* embedded donation widget from give lively */}
-      <Script src="https://secure.givelively.org/widgets/simple_donation/clearviction/giving-tuesday-2023.js?show_suggested_amount_buttons=true&show_in_honor_of=true&address_required=false&has_required_custom_question=false&suggested_donation_amounts[]=10&suggested_donation_amounts[]=25&suggested_donation_amounts[]=50&suggested_donation_amounts[]=100%27;document.getElementsByTagName(%27head%27)[0].appendChild(gl)" />
+      <Script src="https://secure.givelively.org/widgets/simple_donation/clearviction/giving-tuesday-2023.js?show_suggested_amount_buttons=true&show_in_honor_of=true&address_required=false&has_required_custom_question=false&suggested_donation_amounts[]=10&suggested_donation_amounts[]=25&suggested_donation_amounts[]=50&suggested_donation_amounts[]=100&suggested_donation_amounts[]=150;document.getElementsByTagName(%27head%27)[0].appendChild(gl)" />
       <div id="give-lively-widget" className="gl-simple-donation-widget" />
     </Box>
   );
