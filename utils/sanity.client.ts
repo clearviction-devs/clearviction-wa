0b362333e5a4417031af0ec1730b9e48f06433a3
Eleni Papanicolas <elenipapanicolas@gmail.com>
2023-12-27 13:51:53 -0500

    Add felony queries and clarify names for misdemeanor

--- utils/sanity.client.ts
+++ utils/sanity.client.ts
@@ -3,8 +3,10 @@ import { createClient } from 'next-sanity';
 import { apiVersion, dataset, projectId } from './sanity.api.ts';
 import {
   calculatorConfigQuery,
-  calculatorPagePaths,
-  calculatorPagesBySlugQuery,
+  calculatorFelonyPagePaths,
+  calculatorFelonyPagesBySlugQuery,
+  calculatorMisdemeanorPagePaths,
+  calculatorMisdemeanorPagesBySlugQuery,
 } from './sanity.queries.ts';
 
 const sanityClient = (token?: string) => {
@@ -18,18 +20,32 @@ const sanityClient = (token?: string) => {
   return projectId ? createClientCall : null;
 };
 
-export async function getCalculatorPageBySlug({
+export async function getCalculatorMisdemeanorPageBySlug({
   slug,
   token,
 }: {
   slug: string;
   token?: string;
 }) {
-  return sanityClient(token)?.fetch(calculatorPagesBySlugQuery, { slug });
+  return sanityClient(token)?.fetch(calculatorMisdemeanorPagesBySlugQuery, { slug });
 }
 
-export async function getCalculatorPagePaths(): Promise<string[] | undefined> {
-  return sanityClient()?.fetch(calculatorPagePaths);
+export async function getCalculatorMisdemeanorPagePaths(): Promise<string[] | undefined> {
+  return sanityClient()?.fetch(calculatorMisdemeanorPagePaths);
+}
+
+export async function getCalculatorFelonyPageBySlug({
+  slug,
+  token,
+}: {
+  slug: string;
+  token?: string;
+}) {
+  return sanityClient(token)?.fetch(calculatorFelonyPagesBySlugQuery, { slug });
+}
+
+export async function getCalculatorFelonyPagePaths(): Promise<string[] | undefined> {
+  return sanityClient()?.fetch(calculatorFelonyPagePaths);
 }
 
 export async function getCalculatorConfig() {
