b57369deae04767071294df57ad8c43989b86805
Emily Turner <149205316+emilyemilyemilyemilyemilyemily@users.noreply.github.com>
2024-02-22 16:38:17 -0800

    Tf 10645/create contact form handler (#171)

---
+++ lambda/contact_form/contact_form_handler.py
@@ -0,0 +1,52 @@
+import json
+import boto3
+
+ses = boto3.client("ses", region_name="us-west-2")
+
+SENDER = "info@clearviction.org"
+RECEIVER = "info@clearviction.org"
+
+
+def lambda_handler(event, context):
+    send_email(event)
+    response = {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "isBase64Encoded": False,
+        "body": json.dumps({"result": "Success"}),
+    }
+    return response
+
+
+def send_email(event):
+    try:
+        params = {
+            "Destination": {"ToAddresses": [RECEIVER]},
+            "Message": {
+                "Body": {
+                    "Text": {
+                        "Data": "Name: "
+                        + event["name"]
+                        + "\nEmail: "
+                        + event["email"]
+                        + "\nContact Type: "
+                        + event["contact_type"]
+                        + "\nMessage: "
+                        + event["message"],
+                        "Charset": "UTF-8",
+                    }
+                },
+                "Subject": {
+                    "Data": "Website Contact Form: " + event["name"],
+                    "Charset": "UTF-8",
+                },
+            },
+            "Source": SENDER,
+        }
+        ses.send_email(**params)
+    except Exception as e:
+        print("Error sending email: ", e)
+        raise e
---
+++ lambda/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="contact_form",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=["boto3", "moto"],
+)
