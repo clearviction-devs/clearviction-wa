bcb8210a1789e43c8c8e17485f0cd93f7247c0ff
Stephan D Ramalho <112018660+sramalho94@users.noreply.github.com>
2024-03-03 23:33:32 -0500

    S53 11843/felony branch progress bar (#206)

--- components/calculator/CalcStepper.tsx
+++ components/calculator/CalcStepper.tsx
@@ -8,7 +8,7 @@ import { useEffect } from 'react';
 
 import theme from '../../styles/themes/theme.tsx';
 
-const steps = [
+const defaultSteps = [
   'your offense',
   'surrounding circumstances',
   'terms of offense',
@@ -16,11 +16,16 @@ const steps = [
 
 export default function HorizontalLinearStepper() {
   const [activeStep, setActiveStep] = React.useState(0);
+  const [isProPath, setIsProPath] = React.useState(false);
+
+  const steps = isProPath ? defaultSteps.slice(0, 2) : defaultSteps;
 
   const matchesXS = useMediaQuery(theme.breakpoints.down('sm'));
 
   const handleNext = () => {
     const { pathname } = window.location;
+
+    if (pathname.includes('classcpro') || pathname.includes('classbpro')) setIsProPath(true);
 
     type Items = {
       [key: string]: number;
