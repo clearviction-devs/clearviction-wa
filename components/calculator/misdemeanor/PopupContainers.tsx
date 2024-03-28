0c75fcc47c366193820452b6b5ea8a56dac7b612
Leo "Funa" Funabashi <leo.funa@gmail.com>
2024-03-28 18:04:15 -0300

    Tf 12500/test plan with test refactor (#231)

--- components/calculator/misdemeanor/PopupContainers.tsx
+++ components/calculator/misdemeanor/PopupContainers.tsx
@@ -21,6 +21,7 @@ export default function NotSurePopup({ calculatorConfig, openNotSurePopup, setOp
       onClose={() => setOpenNotSurePopup(false)}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
+      data-cy="not-sure-popup"
     >
       <DialogTitle id="alert-dialog-title">
         {calculatorConfig.notSureAnswer.header}
@@ -32,7 +33,7 @@ export default function NotSurePopup({ calculatorConfig, openNotSurePopup, setOp
         />
       </DialogContent>
       <DialogActions>
-        <Button onClick={() => setOpenNotSurePopup(false)}>
+        <Button onClick={() => setOpenNotSurePopup(false)} data-cy="not-sure-pop-up-close">
           {calculatorConfig.notSureAnswer.closeText}
         </Button>
       </DialogActions>
@@ -63,6 +64,7 @@ export function ShareCalculatorPopup({ openSharePopup, setOpenSharePopup }: {
       onClose={() => closeDialog()}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
+      data-cy="share-calc-popup"
     >
       <CloseIcon
         color="inherit"
--- cypress/e2e/misdemeanor/fishing/eligible-path.cy.ts
+++ cypress/e2e/misdemeanor/fishing/eligible-path.cy.ts
@@ -2,59 +2,54 @@ describe('From Calculator first question', () => {
   describe('user choosing Eligible options', () => {
     it('can access Fishing Violation initial page', () => {
       cy.visit('/calculator/head-mis-3-cont');
-
-      cy.get('strong:contains("Was this offense a misdemeanor or gross misdemeanor in Washington state?")')
-        .should('be.visible');
-      cy.get('a:visible').contains('Yes').click();
+      cy.get('[data-cy="calc-choice-0"]:visible')
+        .should('have.attr', 'href', '/calculator/head-special-4-cont')
+        .click();
 
       cy.url().should('include', '/calculator/head-special-4-cont');
       cy.get('a:visible')
+        // We are using Match by Text here because we want to make sure that we are entering in
+        // Fishing path, so if btn order change, we dont want to break the test
         .contains('Fishing Violation')
         .as('fishingViolationBtn')
         .should('have.length', 1);
-      cy.get('@fishingViolationBtn').click();
+      cy.get('@fishingViolationBtn')
+        .should('have.attr', 'href', '/calculator/m-offense-fish-1-cont')
+        .click();
 
       cy.url().should('include', '/calculator/m-offense-fish-1-cont');
-      cy.get('h1:visible')
-        .contains(/^Violation of Fishing Regulation$/)
-        .should('have.length', 1);
     });
 
     it('from fishing initial page, can receive Eligible page', () => {
       cy.visit('/calculator/m-offense-fish-1-cont');
-      cy.get('a:visible').contains(/^Continue$/)
+      cy.get('[data-cy="calc-choice-0"]:visible')
         .as('continueBtn')
         .should('have.length', 1);
-      cy.get('@continueBtn').click();
+      cy.get('@continueBtn')
+        .should('have.attr', 'href', '/calculator/m-offense-fish-2-cont')
+        .click();
 
       cy.url().should('include', '/calculator/m-offense-fish-2-cont');
-      cy.get('strong:visible').contains('Are you a member of a tribe?').should('have.length', 1);
-      cy.get('a:visible').contains(/^Yes$/).click();
+      cy.get('[data-cy="calc-choice-0"]:visible')
+        .should('have.attr', 'href', '/calculator/m-offense-fish-3-cont')
+        .click();
 
       cy.url().should('include', '/calculator/m-offense-fish-3-cont');
-      cy.get('strong:visible')
-        .contains('Did you (or your family member) receive a fishing related conviction while exercising a treaty right?')
-        .should('have.length', 1);
-      cy.get('a:visible').contains('Yes').click();
+      cy.get('[data-cy="calc-choice-0"]:visible')
+        .should('have.attr', 'href', '/calculator/m-offense-fish-4-cont')
+        .click();
 
-      cy.url().should('include', '/m-offense-fish-4-cont');
-      cy.get('strong:visible')
-        .contains('Was it before January')
-        .should('have.length', 1);
-      cy.get('a:visible').contains('Yes').click();
+      cy.url().should('include', '/calculator/m-offense-fish-4-cont');
+      cy.get('[data-cy="calc-choice-0"]:visible')
+        .should('have.attr', 'href', '/calculator/m-offense-fish-5-cont')
+        .click();
 
       cy.url().should('include', '/calculator/m-offense-fish-5-cont');
-      cy.get('strong:visible')
-        .contains('Did your tribe (or your family member\'s tribe)')
-        .should('have.length', 1);
-      cy.get('a:visible').contains('Yes').click();
+      cy.get('[data-cy="calc-choice-0"]:visible')
+        .should('have.attr', 'href', '/calculator/m-offense-fish-5-eligible')
+        .click();
 
       cy.url().should('include', '/calculator/m-offense-fish-5-eligible');
-      cy.get('h1:visible')
-        .contains('Your Conviction May Be Eligible to Vacate!')
-        .should('have.length', 1);
     });
-
-    it('on Eligible page, check that important info are working');
   });
 });
