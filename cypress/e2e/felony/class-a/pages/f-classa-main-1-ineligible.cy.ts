b7ee9af1911a824df9c450589174e0cd5324a86e
Kathy Ye <kathyye15@gmail.com>
2024-04-03 14:13:43 -0700

    separate contexts

--- cypress/e2e/felony/class-a/pages/f-classa-main-1-ineligible.cy.ts
+++ cypress/e2e/felony/class-a/pages/f-classa-main-1-ineligible.cy.ts
@@ -1,14 +1,6 @@
 import testIneligiblePage from "../../utils/testIneligiblePage"
 
-describe("Felony Classification Page", () => {
-  it('should redirect to Felony Class A page when choosing Felony Class A option', () => {
-    cy.visit('/calculator/head-felony-4-cont');
-    cy.get('[data-cy="calc-choice-0"]')
-      .should('be.visible')
-      .should('contain', 'Felony Class A')
-      .click();
-    cy.url().should('include', 'calculator/f-classa-main-1-ineligible');
-  })
+describe("Felony Class A Page", () => {
   it('should display ineligibility content due to RCW 9.94A.030', () => {
     testIneligiblePage('f-classa-main-1-ineligible', 'RCW 9.94A.030', 'https://app.leg.wa.gov/RCW/default.aspx?cite=9.94A.030');
   })
