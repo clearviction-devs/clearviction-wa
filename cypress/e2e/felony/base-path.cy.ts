fad546b045cd64fcbf846deef8e055ffe8b787b3
Kathy Ye <kathyye15@gmail.com>
2024-04-02 23:14:22 -0700

    refactor test for navigating to Felony page

--- cypress/e2e/felony/base-path.cy.ts
+++ cypress/e2e/felony/base-path.cy.ts
@@ -1,11 +1,11 @@
-describe('From Calculator first question', () => {
-  describe('user choosing Eligible options', () => {
-    it('can access Felony initial page', () => {
-      cy.visit('/calculator/head-start-3-cont');
-      cy.get('[data-cy="calc-choice-1"]:visible')
-        .should('have.attr', 'href', '/calculator/head-felony-4-cont')
-        .click();
-      cy.url().should('include', '/calculator/head-felony-4-cont');
-    })
+describe('Offense Classification Page', () => {
+  it('should redirect to the Felony page when choosing Felony option', () => {
+    cy.visit('/calculator/head-start-3-cont');
+    cy.get('[data-cy="calc-choice-1"]')
+      .should('be.visible')
+      .should('have.attr', 'href', '/calculator/head-felony-4-cont')
+      .should('contain', 'Felony')
+      .click();
+    cy.url().should('include', '/calculator/head-felony-4-cont');
   })
 })
