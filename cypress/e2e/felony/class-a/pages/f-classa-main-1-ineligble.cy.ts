8cad38b5d331134fe58de86362373af8828ea038
Kathy Ye <kathyye15@gmail.com>
2024-04-03 01:01:36 -0700

    refactor test suite for improved description, organization, and proper redirect action verification

--- cypress/e2e/felony/class-a/pages/f-classa-main-1-ineligble.cy.ts
+++ cypress/e2e/felony/class-a/pages/f-classa-main-1-ineligble.cy.ts
@@ -1,5 +1,15 @@
 import testIneligiblePage from "../../utils/testIneligiblePage"
 
-describe('f-classa-main-1-ineligble', () => {
-  testIneligiblePage('f-classa-main-1-ineligble');
+describe("Felony Classification Page", () => {
+  it('should redirect to Felony Class A page when choosing Felony Class A option', () => {
+    cy.visit('/calculator/head-felony-4-cont');
+    cy.get('[data-cy="calc-choice-0"]')
+      .should('be.visible')
+      .should('contain', 'Felony Class A')
+      .click();
+    cy.url().should('include', 'calculator/f-classa-main-1-ineligble');
+  })
+  it('should display ineligibility content for RCW 9.94A.030', () => {
+    testIneligiblePage('f-classa-main-1-ineligble', 'RCW 9.94A.030', 'https://app.leg.wa.gov/RCW/default.aspx?cite=9.94A.030');
+  })
 })
