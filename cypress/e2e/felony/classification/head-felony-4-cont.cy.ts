4a471e595b186b00331f803f8d016ad670218dae
kathyye15 <kathyye15@gmail.com>
2024-04-04 05:03:02 -0700

    Test felony A calculator screens (#238)
    * test common base path to initial felony screens
    
    * create resusable function testing ineligibility content
    
    * test f-classa-main-1-ineligible page
    
    * refactor test for navigating to Felony page
    
    * refactor test to focus on test specifics related to ineligibility
    
    * refactor test suite for improved description, organization, and proper redirect action verification
    
    * modify description
    
    * update slug
    
    * separate contexts

---
+++ cypress/e2e/felony/classification/head-felony-4-cont.cy.ts
@@ -0,0 +1,37 @@
+describe("Felony Classification Page", () => {
+  beforeEach(() => {
+    cy.visit('/calculator/head-felony-4-cont');
+  })
+
+  it('should redirect to Felony Class A page when choosing Felony Class A option', () => {
+    cy.get('[data-cy="calc-choice-0"]')
+      .should('be.visible')
+      .should('contain', 'Felony Class A')
+      .click();
+    cy.url().should('include', 'calculator/f-classa-main-1-ineligible');
+  })
+  it('should redirect to Felony Class B page when choosing Felony Class B option', () => {
+    cy.get('[data-cy="calc-choice-1"]')
+      .should('be.visible')
+      .should('contain', 'Felony Class B')
+      .click();
+    cy.url().should('include', 'calculator/f-start-classb-1-cont');
+  })
+  it('should redirect to Felony Class C page when choosing Felony Class C option', () => {
+    cy.get('[data-cy="calc-choice-2"]')
+      .should('be.visible')
+      .should('contain', 'Felony Class C')
+      .click();
+    cy.url().should('include', 'calculator/f-start-classc-1-cont');
+  })
+  it('should open "I\'m not sure" modal when choosing "I\'m not sure" option', () => {
+    cy.get('[data-cy="not-sure-button"]')
+      .should('be.visible')
+      .click();
+    cy.get('[data-cy="not-sure-pop-up-close"]')
+      .should('be.visible')
+      .click();
+    cy.get('[data-cy="not-sure-popup"]')
+      .should('not.exist')
+  })
+})
