2dddee2b8017c22a88ce3fe608d806c3b8199e04
Kathy Ye <kathyye15@gmail.com>
2024-04-05 00:30:19 -0700

    Add tests for Felony Class B Page

---
+++ cypress/e2e/felony/class-b/f-start-classb-1-cont.cy.ts
@@ -0,0 +1,32 @@
+describe("Felony Class B Page", () => {
+  beforeEach(() => {
+    cy.visit('/calculator/f-start-classb-1-cont');
+  })
+
+  it('should display "RCW 9.94A.030" as a link to its respective WA State Legislature page', () => {
+    cy.contains('RCW 9.94A.030')
+      .should('have.attr', 'href', 'https://app.leg.wa.gov/rcw/default.aspx?cite=9.94A.030')
+  })
+  it('should redirect users to Felony Class B Yes-Pros Main Page when "yes" button is clicked', () => {
+    cy.get('[data-cy="calc-choice-0"]')
+      .should('be.visible')
+      .click();
+    cy.url().should('include', 'calculator/f-offense-classbpro-1-main');
+  })
+  it('should redirect users to Felony Class B No-Pros Main Page when "no" button is clicked', () => {
+    cy.get('[data-cy="calc-choice-1"]')
+      .should('be.visible')
+      .click();
+    cy.url().should('include', 'calculator/f-offense-classb-1-main');
+  })
+  it('should open "I\'m not sure" modal when choosing "I\'m not sure" option', () => {
+    cy.get('[data-cy="not-sure-button"]')
+      .should('be.visible')
+      .click();
+    cy.get('[data-cy="not-sure-pop-up-close"]')
+      .should('be.visible')
+      .click();
+    cy.get('[data-cy="not-sure-popup"]')
+      .should('not.exist')
+  })
+})
