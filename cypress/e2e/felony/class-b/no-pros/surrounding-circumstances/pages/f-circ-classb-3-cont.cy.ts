bae2c5a1e2e3fe824cb1d9ed619437304723a0b6
Kathy Ye <kathyye15@gmail.com>
2024-04-05 19:19:38 -0700

    Add tests for Felony Class B No-Pros circ-classb-3 pages

---
+++ cypress/e2e/felony/class-b/no-pros/surrounding-circumstances/pages/f-circ-classb-3-cont.cy.ts
@@ -0,0 +1,20 @@
+import testNotSureModal from "../../../../../utils/testNotSureModal";
+
+describe("Felony Class B No-Pros Surrounding Circumstances Third Page", () => {
+  beforeEach(() => {
+    cy.visit('/calculator/f-circ-classb-3-cont');
+  })
+  it('should redirect users to Felony Class B No-Pros Surrounding Circumstances Fourth Page when "yes" button is clicked', () => {
+    cy.get('[data-cy="calc-choice-0"]')
+      .should('be.visible')
+      .click();
+    cy.url().should('include', 'calculator/f-circ-classb-4-cont')
+  })
+  it('should redirect users to Felony Class B No-Pros Ineligible Page when "no" button is clicked', () => {
+    cy.get('[data-cy="calc-choice-1"]')
+      .should('be.visible')
+      .click();
+    cy.url().should('include', 'calculator/f-circ-classb-3-ineligible')
+  })
+  testNotSureModal();
+})
