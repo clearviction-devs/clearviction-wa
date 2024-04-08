31e3fa151b5872e0b6e97632a324c20d24540853
Anthony Tzeng <anthony@Anthonys-MacBook-Pro.local>
2024-04-08 14:18:42 -0700

    Created util testing function for links in finalPage

---
+++ cypress/e2e/utils/finalPageLinks.ts
@@ -0,0 +1,23 @@
+export default function finalPageLinks(path: string) {
+  cy.visit(`/calculator/${path}`)
+    //Feedback button
+    cy.get('[data-cy="feedback-button"]')
+    .should('have.attr', 'href')
+    .and('include', 'forms');
+
+    // Check another Conviction
+  cy.get('[data-cy="check-another-conviction"]')
+    .should('be.visible')
+    .click()
+  cy.url().should('include', '/calculator/head-initial-1-cont')
+
+  // Share Calculator Link
+  cy.get('[data-cy="share-calc-link"]')
+    .should('be.visible')
+    .click()
+  
+  // Share calculator modal
+  cy.get('[data-cy="share-calc-modal"]')
+    .should('have.length', 1)
+    .should('be.visible')
+}
