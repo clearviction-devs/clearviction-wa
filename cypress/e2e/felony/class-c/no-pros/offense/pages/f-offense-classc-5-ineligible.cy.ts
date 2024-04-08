973778abb1a363ee4560c3875b4d11b6d345a530
Anthony Tzeng <anthony@Anthonys-MacBook-Pro.local>
2024-04-08 14:55:47 -0700

    Created tests for f-offense-classc-5-ineligible

--- cypress/e2e/felony/class-c/no-pros/offense/pages/f-offense-classc-5-ineligible.cy.ts
+++ cypress/e2e/felony/class-c/no-pros/offense/pages/f-offense-classc-5-ineligible.cy.ts
@@ -4,12 +4,12 @@ import testPreviousButton from "../../../../../utils/previousButton";
 
 describe('Felony Class C Ineligible Page', () => {
   it('should display ineligibility content due to RCW 9.94A.640(2)(b)', () => {
-    testIneligiblePage('f-offense-classc-4-ineligible', '9.94A.640(2)(b)', 'https://app.leg.wa.gov/RCW/default.aspx?cite=9.94a.640');
+    testIneligiblePage('f-offense-classc-5-ineligible', '9.94A.640(2)(b)', 'https://app.leg.wa.gov/RCW/default.aspx?cite=9.94a.640');
   })
   it('should have the bottom links function properly', () => {
-    finalPageLinks('f-offense-classc-4-ineligible')
+    finalPageLinks('f-offense-classc-5-ineligible')
   })
   it('directs to the previous page when clicked', () => {
-    testPreviousButton('f-offense-classc-4-ineligible')
+    testPreviousButton('f-offense-classc-5-ineligible')
   })
 })
