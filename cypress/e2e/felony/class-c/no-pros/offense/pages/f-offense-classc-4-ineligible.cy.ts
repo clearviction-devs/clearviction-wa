4f1ba052eac773a5191e84eaede4eeca6bc867d4
Anthony Tzeng <anthony@Anthonys-MacBook-Pro.local>
2024-04-08 14:18:06 -0700

    Added finalPageLinks testing

--- cypress/e2e/felony/class-c/no-pros/offense/pages/f-offense-classc-4-ineligible.cy.ts
+++ cypress/e2e/felony/class-c/no-pros/offense/pages/f-offense-classc-4-ineligible.cy.ts
@@ -1,7 +1,11 @@
 import testIneligiblePage from "../../../../../utils/testIneligiblePage";
+import finalPageLinks from "../../../../../utils/finalPageLinks";
 
 describe("Felony Class C Ineligible Page", () => {
   it('should display ineligibility content due to RCW 9.94A.640(2)(b)', () => {
     testIneligiblePage('f-offense-classc-4-ineligible', '9.94A.640(2)(b)', 'https://app.leg.wa.gov/RCW/default.aspx?cite=9.94a.640');
+  })
+  it('should have the bottom links function properly', () => {
+    finalPageLinks('f-offense-classc-4-ineligible')
   })
 })
