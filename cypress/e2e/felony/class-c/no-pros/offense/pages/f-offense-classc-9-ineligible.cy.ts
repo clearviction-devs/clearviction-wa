0f57ecbeae0e821cec40105de2daa93ec8f0ddb1
Anthony <aitzeng@gmail.com>
2024-04-09 23:54:00 -0700

    Added tests for f-offense-classc-10-cont and f-offense-classc-9-ineligible

---
+++ cypress/e2e/felony/class-c/no-pros/offense/pages/f-offense-classc-9-ineligible.cy.ts
@@ -0,0 +1,15 @@
+import testIneligiblePage from "../../../../../utils/testIneligiblePage";
+import finalPageLinks from "../../../../../utils/finalPageLinks";
+import testPreviousButton from "../../../../../utils/previousButton";
+
+describe('Felony Class C Ineligible Page', () => {
+  it('should display ineligibility content due to RCW 9.94A.640(2)(b)', () => {
+    testIneligiblePage('f-offense-classc-9-ineligible', '9.94A.640(2)(b)', 'https://app.leg.wa.gov/RCW/default.aspx?cite=9.94a.640');
+  })
+  it('should have the bottom links function properly', () => {
+    finalPageLinks('f-offense-classc-9-ineligible')
+  })
+  it('directs to the previous page when clicked', () => {
+    testPreviousButton('f-offense-classc-9-ineligible')
+  })
+})
