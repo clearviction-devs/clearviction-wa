6a5f8d169bc3727ebcde19c16262e12a04daccae
Anthony <aitzeng@gmail.com>
2024-04-09 23:49:36 -0700

    Added tests for f-offense-classc-9-cont

---
+++ cypress/e2e/felony/class-c/no-pros/offense/pages/f-offense-classc-9-cont.cy.ts
@@ -0,0 +1,32 @@
+import testNotSureModal from "../../../../../utils/testNotSureModal";
+import testPreviousButton from "../../../../../utils/previousButton";
+
+describe('f-offense-classc-9-cont', () => {
+  beforeEach(() => {
+    cy.visit('/calculator/f-offense-classc-9-cont')
+  })
+  it('contains the proper content', () => {
+    cy.get('[data-cy="calc-block-of-content"]')
+    .should('be.visible')
+    .should('have.length', 1)
+  })
+  it('displays 9.94A.640(2)(b) and directs to the proper link', () => {
+    cy.contains('9.94A.640(2)(b)')
+      .should('be.visible')
+      .should('have.attr', 'href', 'https://app.leg.wa.gov/RCW/default.aspx?cite=9.94a.640')
+  })
+  it('directs to f-offense-classc-10-cont when clicking No', () => {
+    cy.get('[data-cy="calc-choice-1')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', '/calculator/f-offense-classc-10-cont')
+  })
+  it('directs to f-offense-classc-9-ineligible when clicking Yes', () => {
+    cy.get('[data-cy="calc-choice-0')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', '/calculator/f-offense-classc-9-ineligible')
+  })
+  testNotSureModal();
+  testPreviousButton('f-offense-classc-9-cont')
+})
