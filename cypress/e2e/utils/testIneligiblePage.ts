7770c2361f6781eba3c2081ec3f542e33f71fb4c
Anthony <aitzeng@gmail.com>
2024-04-10 00:07:13 -0700

    Added test to confirm RCW

--- cypress/e2e/utils/testIneligiblePage.ts
+++ cypress/e2e/utils/testIneligiblePage.ts
@@ -2,6 +2,8 @@ export default function testIneligiblePage(path: string, rcwAnchorText: string, 
   cy.visit(`/calculator/${path}`);
   cy.contains('not eligible to vacate')
     .should('be.visible')
+  cy.contains(`${rcwAnchorText}`)
+    .should('be.visible')
   cy.contains(rcwAnchorText)
     .should('have.attr', 'href', rcwHref);
 }
