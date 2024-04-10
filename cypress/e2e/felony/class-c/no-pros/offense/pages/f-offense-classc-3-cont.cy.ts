9840398235e0a0597b84d511624463e3538b83db
Anthony <aitzeng@gmail.com>
2024-04-09 23:28:03 -0700

    Updated test to include content and button

--- cypress/e2e/felony/class-c/no-pros/offense/pages/f-offense-classc-3-cont.cy.ts
+++ cypress/e2e/felony/class-c/no-pros/offense/pages/f-offense-classc-3-cont.cy.ts
@@ -15,4 +15,23 @@ describe('f-offense-classc-3-cont', () => {
       .should('be.visible')
       .should('have.attr','href','https://apps.leg.wa.gov/rcw/default.aspx?cite=43.43.830')
   })
+  it('displays 9.94A.640(2)(b) and directs to the proper link', () => {
+    cy.contains('9.94A.640(2)(b)')
+      .should('be.visible')
+      .should('have.attr', 'href', 'https://app.leg.wa.gov/RCW/default.aspx?cite=9.94a.640')
+  })
+  it('directs to f-offense-classc-11-cont when clicking No', () => {
+    cy.get('[data-cy="calc-choice-1')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', '/calculator/f-offense-classc-11-cont')
+  })
+  it('directs to f-offense-classc-6-cont when clicking Yes', () => {
+    cy.get('[data-cy="calc-choice-0')
+      .should('be.visible')
+      .click()
+    cy.url().should('include', '/calculator/f-offense-classc-6-cont')
+  })
+  testNotSureModal();
+  testPreviousButton('f-offense-classc-3-cont')
 })
