d89d02c7e963da49d8fb4b9ad36591ec30a199a6
Kathy Ye <kathyye15@gmail.com>
2024-04-10 19:47:54 -0700

    fix linter issues

--- cypress/e2e/felony/class-b/no-pros/surrounding-circumstances/pages/f-circ-classb-3-cont.cy.ts
+++ cypress/e2e/felony/class-b/no-pros/surrounding-circumstances/pages/f-circ-classb-3-cont.cy.ts
@@ -1,20 +1,20 @@
-import testNotSureModal from "../../../../../utils/testNotSureModal";
+import testNotSureModal from '../../../../../utils/testNotSureModal';
 
-describe("Felony Class B No-Pros Surrounding Circumstances Third Page", () => {
+describe('Felony Class B No-Pros Surrounding Circumstances Third Page', () => {
   beforeEach(() => {
     cy.visit('/calculator/f-circ-classb-3-cont');
-  })
+  });
   it('should redirect users to Felony Class B No-Pros Surrounding Circumstances Fourth Page when "yes" button is clicked', () => {
     cy.get('[data-cy="calc-choice-0"]')
       .should('be.visible')
       .click();
-    cy.url().should('include', 'calculator/f-circ-classb-4-cont')
-  })
+    cy.url().should('include', 'calculator/f-circ-classb-4-cont');
+  });
   it('should redirect users to Felony Class B No-Pros Ineligible Page when "no" button is clicked', () => {
     cy.get('[data-cy="calc-choice-1"]')
       .should('be.visible')
       .click();
-    cy.url().should('include', 'calculator/f-circ-classb-3-ineligible')
-  })
+    cy.url().should('include', 'calculator/f-circ-classb-3-ineligible');
+  });
   testNotSureModal();
-})
+});
