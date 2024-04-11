4abb04070c2e20b7584ed010eaf87ee33917bda1
Eleni Papanicolas <eleni.papanicolas@proton.me>
2024-04-11 13:54:48 -0400

    fix share links test

--- cypress/e2e/utils/testFinalPageLinks.ts
+++ cypress/e2e/utils/testFinalPageLinks.ts
@@ -12,9 +12,8 @@ export default function testFinalPageLinks(path: string) {
   cy.url().should('include', '/calculator/head-initial-1-cont');
 
   // Share Calculator Link
-  cy.get('[data-cy="share-calc-link"]')
-    .should('be.visible')
-    .click();
+  cy.get('[data-cy="share-calc-link"]').as('btn');
+  cy.get('@btn').click();
 
   // Share calculator modal
   cy.get('[data-cy="share-calc-modal"]')
