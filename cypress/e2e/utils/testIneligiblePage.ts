85d620ceb69a664f7ff590113b585d2ffac91f30
kathyye15 <kathyye15@gmail.com>
2024-04-10 22:19:14 -0700

    Chain 'should' assertions for improved readability in Cypress tests
    Signed-off-by: kathyye15 <kathyye15@gmail.com>

--- cypress/e2e/utils/testIneligiblePage.ts
+++ cypress/e2e/utils/testIneligiblePage.ts
@@ -2,8 +2,7 @@ export default function testIneligiblePage(path: string, rcwAnchorText: string, 
   cy.visit(`/calculator/${path}`);
   cy.contains('not eligible to vacate')
     .should('be.visible')
-  cy.contains(`${rcwAnchorText}`)
-    .should('be.visible')
   cy.contains(rcwAnchorText)
+    .should('be.visible')
     .should('have.attr', 'href', rcwHref);
 }
