25bbb30976b2de1c2ba5aaedde23eb1efa418a46
kathyye15 <kathyye15@gmail.com>
2024-06-26 07:50:02 -0700

    implement sidebar

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,11 +1,19 @@
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import MenuIcon from '@mui/icons-material/Menu';
 import {
   AppBar,
   Box,
   Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
 } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import Link from 'next/link';
 import React, { useState } from 'react';
 
 import navItems from '../../content/navItems.ts';
@@ -22,24 +30,87 @@ export default function Header() {
     setDrawerOpen(!drawerOpen);
   };
 
+  const drawer = (
+    <Box onClick={handleDrawerToggle}>
+      <Box>
+        <IconButton
+          aria-label="Close navigation"
+          sx={{
+            marginRight: 'auto',
+          }}
+        >
+          <ArrowForwardIosIcon
+            fontSize="large"
+            sx={{
+              color: theme.palette.text.light,
+            }}
+          />
+        </IconButton>
+      </Box>
+      <List className="nav-mobile" sx={{ transform: 'translateY(-20px)' }}>
+        {navItems
+          .map(({ href, text, sublist }) => (
+            <>
+              <ListItem key={text}>
+                <ListItemButton
+                  component={Link}
+                  href={href}
+                >
+                  <ListItemText
+                    primary={text}
+                    primaryTypographyProps={{ style: { fontSize: '16px', fontWeight: '700' } }}
+                  />
+                </ListItemButton>
+              </ListItem>
+              <List sx={{ paddingLeft: '32px' }}>
+                {sublist?.map((item) => (
+                  <ListItem key={item} disablePadding>
+                    <ListItemButton sx={{ paddingTop: '8px', paddingBottom: '8px', paddingLeft: '8px' }}>
+                      <ListItemText
+                        primary={item}
+                        primaryTypographyProps={{
+                          style: { fontSize: '16px', fontWeight: '500' },
+                        }}
+                      />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </List>
+            </>
+          ))}
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <EligibilityButton />
+        </Box>
+      </List>
+    </Box>
+  );
+
   return (
     <AppBar id="main-header" className="nav-desktop" color="primary" elevation={0} component="nav" position="sticky">
       <Box
+        component="nav"
         sx={{
           display: 'flex', alignItems: 'center', justifyContent: 'space-between', height: '80px', position: 'relative', px: 10.5,
         }}
       >
-        <Box component="nav" sx={{ width: '100%' }}>
+        <Drawer
+          open={drawerOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
+          anchor="right"
+        >
+          {drawer}
+        </Drawer>
+        <Box sx={{ width: '100%' }}>
           {matches && (
-            <Box id="box1" sx={{ display: 'flex', width: '100%' }}>
+            <IconButton aria-label="open sidebar menu" onClick={handleDrawerToggle} sx={{ display: 'flex', width: '100%' }}>
               <MenuIcon
                 sx={{
                   color: theme.palette.text.light, marginLeft: 'auto',
                 }}
                 fontSize="large"
-                onClick={handleDrawerToggle}
               />
-            </Box>
+            </IconButton>
           )}
           {!matches && (
             <Box
@@ -52,9 +123,6 @@ export default function Header() {
               }}
             >
               {navItems
-                .filter(
-                  (item) => item.text !== 'Access Calculator' && item.text !== 'Donate',
-                )
                 .map((item) => (
                   <Button
                     key={item.text}
