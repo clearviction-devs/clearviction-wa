e03176e7f5ec06df816a167fc98bfa79677c030f
Eleni Papanicolas <eleni.papanicolas@proton.me>
2024-07-06 11:24:47 -0400

    update calc styling on progress bar final page and add progress bar to end page

--- components/calculator/CalcHeader.tsx
+++ components/calculator/CalcHeader.tsx
@@ -23,6 +23,7 @@ function CustomHorizontalStepper() {
   const [activeStep, setActiveStep] = React.useState(0);
   const [isProPath, setIsProPath] = React.useState(false);
   const [isMisSpecialCase, setIsMisSpecialCase] = React.useState(false);
+  const [isEndPage, setIsEndPage] = React.useState(false);
 
   const steps = isProPath ? defaultSteps.slice(0, 2) : defaultSteps;
 
@@ -34,6 +35,8 @@ function CustomHorizontalStepper() {
     if (pathname.includes('m-offense-pro') || pathname.includes('m-offense-mari') || pathname.includes('m-offense-fish')) setIsMisSpecialCase(true);
 
     if (pathname.includes('m-offense-main-1-cont')) setIsMisSpecialCase(false);
+
+    if (pathname.includes('eligible') || pathname.includes('ineligible')) setIsEndPage(true);
 
     type Items = {
       [key: string]: number;
@@ -57,11 +60,6 @@ function CustomHorizontalStepper() {
   const getBackgroundColor = (label: string) => {
     const isActiveStep = steps[activeStep] === label;
     const isCompleted = steps.indexOf(label) < activeStep;
-    // const isFinalStep = steps.indexOf(label) === steps.length - 1;
-
-    // if (isFinalStep) {
-    //   return 'transparent';
-    // }
 
     return (isActiveStep || isCompleted)
       ? theme.palette.secondary.dark : theme.palette.secondary.main;
@@ -74,22 +72,62 @@ function CustomHorizontalStepper() {
         {!isMisSpecialCase && steps.map((label) => {
           const stepProps: { completed?: boolean } = {};
 
+          const isFinalStep = steps.indexOf(label) === steps.length - 1;
+
+          const stepSxProps = {
+            height: 4,
+            borderRadius: 50,
+            backgroundColor: getBackgroundColor(label),
+            width: '100%',
+          };
+
           return (
-            <Step
+            <Box
               key={label}
-              {...stepProps}
               sx={{
+                display: 'flex',
+                width: '100%',
                 height: 4,
                 borderRadius: 50,
                 margin: 1,
-                backgroundColor: getBackgroundColor(label),
               }}
-            />
+            >
+              {/* Displays the steps that are NOT the final step, including the end pages */}
+              <Step
+                {...stepProps}
+                sx={{
+                  ...stepSxProps,
+                  display: (!isEndPage && isFinalStep) ? 'none' : 'block',
+                }}
+              />
+
+              {/* Displays just the final step so that it can have special styling */}
+              <Step
+                {...stepProps}
+                sx={{
+                  ...stepSxProps,
+                  display: (!isEndPage && isFinalStep) ? 'block' : 'none',
+                  borderTopRightRadius: 0,
+                  borderBottomRightRadius: 0,
+                }}
+              />
+              <Box sx={{
+                display: (!isEndPage && isFinalStep) ? 'block' : 'none',
+                borderTopRightRadius: 50,
+                borderBottomRightRadius: 50,
+                borderTopLeftRadius: 0,
+                borderBottomLeftRadius: 0,
+                backgroundColor: theme.palette.secondary.main,
+                width: '15%',
+              }}
+              />
+
+            </Box>
           );
         })}
       </Stepper>
       <Typography variant="caption" sx={{ color: theme.palette.secondary.dark, ml: 1 }}>
-        {steps[activeStep]}
+        {!isEndPage && steps[activeStep]}
       </Typography>
 
     </Box>
@@ -102,8 +140,7 @@ export default function CalcHeader({ page }:
   const isPageIncludedInStepper = () => {
     const excludedPageSlugs = ['start', 'head'];
     const isPartOfHead = excludedPageSlugs.some((slug) => page.slug.includes(slug));
-    const { isFinalPage } = page;
-    return !(isFinalPage || isPartOfHead);
+    return !isPartOfHead;
   };
 
   return (
