bfe293d7325633798220f0f5e1e0439bd813f331
kathyye15 <kathyye15@gmail.com>
2024-08-10 21:14:12 -0700

    ensure Footer component is unified in HTML structure across all screen sizes

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -34,26 +34,30 @@ function Footer({ isCalc }: FooterProps) {
           }}
         >
           <Box>
-            <Typography className="warning" variant="footerTitle">Conviction Vacation Initiative is an open source project</Typography>
-            <Typography className="warning" variant="footerCaption" sx={{ mt: 5 }} paragraph>
-              {/* eslint-disable-next-line max-len */}
-              We are an open source initiative that emerged following Washington State's New Hope Act.
-            </Typography>
-            <Typography className="warning" variant="footerCaption" paragraph>
-              {/* eslint-disable-next-line max-len */}
-              Formerly known as Clearviction, our project has transitioned to an open-source model, inviting collaboration so we can better serve our community.
-            </Typography>
-            <Typography className="warning" variant="footerCaption" sx={{ mb: 0 }} paragraph>
-              If you would like to contribute to the project, please reach out on
-              {' '}
-              <Link href="https://github.com/clearviction-devs" target="_blank" rel="noopener noreferrer">Github</Link>
-              .
-            </Typography>
+            <Stack>
+              <Typography className="warning" variant="footerTitle">Conviction Vacation Initiative is an open source project</Typography>
+              <Typography className="warning" variant="footerCaption" sx={{ mt: 5 }} paragraph>
+                {/* eslint-disable-next-line max-len */}
+                We are an open source initiative that emerged following Washington State's New Hope Act.
+              </Typography>
+              <Typography className="warning" variant="footerCaption" paragraph>
+                {/* eslint-disable-next-line max-len */}
+                Formerly known as Clearviction, our project has transitioned to an open-source model, inviting collaboration so we can better serve our community.
+              </Typography>
+              <Typography className="warning" variant="footerCaption" sx={{ mb: 0 }} paragraph>
+                If you would like to contribute to the project, please reach out on
+                {' '}
+                <Link href="https://github.com/clearviction-devs" target="_blank" rel="noopener noreferrer">Github</Link>
+                .
+              </Typography>
+            </Stack>
           </Box>
           <Box>
-            <Divider sx={{ borderBottom: `1px solid ${theme.palette.primary.main}`, my: 3 }} />
-            <Typography className="warning" variant="footerDisclaimers" display="block" gutterBottom>LEGAL DISCLAIMER</Typography>
-            <Typography className="warning" variant="footerDisclaimers" display="block" gutterBottom>PRIVACY NOTICE</Typography>
+            <Stack>
+              <Divider sx={{ borderBottom: `1px solid ${theme.palette.primary.main}`, my: 3 }} />
+              <Typography className="warning" variant="footerDisclaimers" display="block" gutterBottom>LEGAL DISCLAIMER</Typography>
+              <Typography className="warning" variant="footerDisclaimers" display="block" gutterBottom>PRIVACY NOTICE</Typography>
+            </Stack>
           </Box>
         </Stack>
       ) : (
