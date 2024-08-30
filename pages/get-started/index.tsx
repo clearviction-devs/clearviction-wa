e29e28f69b29648f6b0ab1b9e5315b0fb21cb92d
Eleni Papanicolas <eleni.papanicolas@proton.me>
2024-08-30 13:25:06 -0400

    create new get-started index page

--- pages/get-started/index.tsx
+++ pages/get-started/index.tsx
@@ -1,243 +1,84 @@
 import {
-  Button, ButtonGroup, Grid, List, ListItem, SxProps, Typography,
+  Container, ContainerProps, Typography,
 } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
-import MuiMarkdown from 'mui-markdown';
 import React from 'react';
 
-import useScroll from '../../components/functional/CustomScroll.tsx';
-import externalLinks from '../../components/functional/ExternalLinks.tsx';
-import IndividualPageHead from '../../components/helper/IndividualPageHead.tsx';
 import AccordionBuilder from '../../components/layout/AccordionBuilder.tsx';
-import FactCard from '../../components/layout/FactCard.tsx';
-import GetStartedStep from '../../components/layout/GetStartedStep.tsx';
-import HeroBanner from '../../components/layout/HeroBanner.tsx';
-import ImageContainer from '../../components/layout/ImageContainer.tsx';
-import SectionContainer, { sectionContainerSxProps } from '../../components/layout/SectionContainer.tsx';
+import GetStartedCard from '../../components/layout/GetStartedCard.tsx';
 import content from '../../content/get-started.ts';
 
-const newSectionContainerSxProps: SxProps = {
-  ...sectionContainerSxProps, px: 0, textAlign: 'left',
-};
+function GSContainer({
+  children,
+  id,
+  sx,
+  ...props
+}: ContainerProps) {
+  return (
+    <Container
+      component="section"
+      id={id}
+      disableGutters
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '24px',
+        maxWidth: {
+          md: '936px',
+        },
+        padding: {
+          xs: '72px 24px 52px 24px',
+          sm: '72px 24px 52px 24px',
+          md: '72px 64px 52px 64px',
+          lg: '112px 32px 72px 32px',
+          xl: '112px 32px 72px 32px',
+        },
+        ...sx,
+      }}
+      {...props}
+    >
+      {children}
+    </Container>
+  );
+}
 
 export default function GetStartedPage() {
-  const theme = useTheme();
-  const matchesXS = useMediaQuery(theme.breakpoints.down('sm'));
-  const { handleLinkClick } = useScroll();
-
-  externalLinks();
-
   return (
     <>
-      <IndividualPageHead
-        title={content.meta.title}
-        metaContent={content.meta.content}
-      />
-
-      <HeroBanner
-        header={content.hero.header}
-      />
-
-      <SectionContainer>
-        <ButtonGroup
-          variant="text"
-          fullWidth
-          orientation={matchesXS ? 'vertical' : 'horizontal'}
-        >
-          {
-            content.buttons.map((button) => (
-              <Button
-                key={button.href}
-                data-href={button.href}
-                onClick={handleLinkClick}
-              >
-                {button.name}
-              </Button>
-            ))
-          }
-        </ButtonGroup>
-      </SectionContainer>
-
-      <SectionContainer id="step-1-documents">
-        <GetStartedStep
-          title={content.steps[0].title}
-          bodyText={content.steps[0].body}
-        >
-
-          <SectionContainer sx={{
-            ...newSectionContainerSxProps,
-          }}
-          >
-            <Typography variant="h5">{content.steps[0].data[0]}</Typography>
-            <Grid container spacing={8} sx={{ my: 1 }}>
-              {content.needToKnowFacts.map((fact) => (
-                <FactCard
-                  key={fact.id}
-                  details={fact.details}
-                  icon={fact.icon}
-                  border={false}
-                />
-              ))}
-            </Grid>
-          </SectionContainer>
-
-          <SectionContainer sx={{
-            ...newSectionContainerSxProps,
-          }}
-          >
-            <Typography variant="h3" sx={{ mb: 4 }}>{content.steps[0].data[1]}</Typography>
-            <MuiMarkdown>
-              {content.steps[0].data[2]}
-            </MuiMarkdown>
-            <Grid container spacing={2} sx={{ my: 3 }}>
-              {content.CHRIMethods.map((method) => (
-                <Grid key={method.id} item xs={12} md={6}>
-                  <AccordionBuilder
-                    id={method.id}
-                    summary={method.summary}
-                    details={method.details}
-                    sx={{ py: 2, height: '6em' }}
-                  />
-                </Grid>
-              ))}
-            </Grid>
-          </SectionContainer>
-
-        </GetStartedStep>
-      </SectionContainer>
-
-      <SectionContainer id="step-2-eligibility">
-        <GetStartedStep
-          title={content.steps[1].title}
-          bodyText={content.steps[1].body}
-          ctaText={content.steps[1].ctaText}
-          ctaLink={content.steps[1].ctaLink}
-          ariaLabels={{ ctaButton: content.steps[1].ariaLabels?.ctaButton }}
-        >
-
-          <SectionContainer>
-            <Grid container sx={{ alignItems: 'center' }}>
-              <ImageContainer
-                alt=""
-                src="/quickstartupdate.svg"
-                style={{ padding: '1em', width: '100%' }}
-                width={862}
-                height={410}
-                useImageDimensions
-              />
-            </Grid>
-          </SectionContainer>
-
-        </GetStartedStep>
-      </SectionContainer>
-
-      <SectionContainer id="step-3-file-with-court">
-        <GetStartedStep
-          title={content.steps[2].title}
-          bodyText={content.steps[2].body}
-        >
-          <Grid container spacing={5} sx={{ mb: 8, mt: 0 }}>
-            {content.step3Substeps.map((step3Substep) => (
-              <FactCard
-                className={`fact-card-${step3Substep.id}`}
-                key={step3Substep.id}
-                title={`Step ${step3Substep.id.slice(-1)}`}
-                details={step3Substep.details}
-                gridItemSize={{ xs: 12, sm: 6, md: 4 }}
-                sxProps={{ height: '100%', justifyContent: 'flex-start' }}
-              />
-            ))}
-          </Grid>
-          <MuiMarkdown>
-            {content.steps[2].data[0]}
-          </MuiMarkdown>
-          <Typography variant="h5" sx={{ mt: 4, mb: 3 }}>{content.steps[2].data[1]}</Typography>
-          <List sx={{ py: 0, pl: 4 }}>
-            {content.rejectionReasons.map((item) => (
-              <ListItem
-                sx={{ display: 'list-item', listStyleType: 'disc', px: 0 }}
-                key={item.id}
-              >
-                {item.details}
-              </ListItem>
-            ))}
-          </List>
-        </GetStartedStep>
-      </SectionContainer>
-
-      <SectionContainer id="step-4-court-hearing">
-        <GetStartedStep
-          title={content.steps[3].title}
-          bodyText={content.steps[3].body}
-        >
-
-          <SectionContainer sx={{
-            ...newSectionContainerSxProps, my: 5,
-          }}
-          >
-            <Typography variant="h3" sx={{ mb: 5 }}>{content.stepFourSectionNames[0]}</Typography>
-            <MuiMarkdown>
-              {content.steps[3].data[0]}
-            </MuiMarkdown>
-            <Typography variant="subtitle2" sx={{ fontWeight: 'bold' }}>{content.steps[3].data[1]}</Typography>
-            <Typography variant="body2" sx={{ my: 2 }}>
-              {content.steps[3].data[2]}
-            </Typography>
-            <Grid container spacing={2} sx={{ my: 3 }}>
-              {content.legalAidForms.map((legalForm) => (
-                <Grid key={legalForm.id} item xs={12} sm={6} md={4}>
-                  <AccordionBuilder
-                    id={legalForm.id}
-                    summary={legalForm.summary}
-                    details={legalForm.details}
-                    sx={{ py: 2, height: '6em' }}
-                  />
-                </Grid>
-              ))}
-            </Grid>
-          </SectionContainer>
-
-          <SectionContainer sx={{
-            ...newSectionContainerSxProps, my: 5,
-          }}
-          >
-            <Typography variant="h3" sx={{ mb: 2 }}>{content.stepFourSectionNames[1]}</Typography>
-            <Grid container spacing={2} sx={{ my: 1 }}>
-              {content.financialAidForms.map((financialForm) => (
-                <Grid key={financialForm.id} item xs={12} sm={6} md={4}>
-                  <AccordionBuilder
-                    id={financialForm.id}
-                    summary={financialForm.summary}
-                    details={financialForm.details}
-                    sx={{ py: 2, height: '6em' }}
-                  />
-                </Grid>
-              ))}
-            </Grid>
-          </SectionContainer>
-
-          <SectionContainer sx={{
-            ...newSectionContainerSxProps, my: 5,
-          }}
-          >
-            <Typography variant="h3" sx={{ mb: 2 }}>{content.stepFourSectionNames[2]}</Typography>
-            <Grid container spacing={2} sx={{ my: 1 }}>
-              {content.eligibilityForms.map((eligibilityForm) => (
-                <Grid key={eligibilityForm.id} item xs={12} sm={6} md={4}>
-                  <AccordionBuilder
-                    id={eligibilityForm.id}
-                    summary={eligibilityForm.summary}
-                    details={eligibilityForm.details}
-                    sx={{ py: 2, height: '6em' }}
-                  />
-                </Grid>
-              ))}
-            </Grid>
-          </SectionContainer>
-
-        </GetStartedStep>
-      </SectionContainer>
+      <GSContainer
+        id="section-get-started-header"
+      >
+        <Typography variant="h1">
+          {content.hero.header}
+        </Typography>
+        <Typography variant="body1">
+          {content.hero.subheading}
+        </Typography>
+      </GSContainer>
+      <GSContainer
+        id="section-get-started-cards"
+        sx={{
+          padding: '44px 24px',
+          margin: 'auto',
+          width: 'auto',
+        }}
+      >
+        {content.steps.map((step) => (
+          <GetStartedCard key={step.overline} {...step} />
+        ))}
+      </GSContainer>
+      <GSContainer>
+        <Typography variant="h3">
+          Vacatable Convictions FAQs
+        </Typography>
+        {content.FAQ.map((item) => (
+          <AccordionBuilder
+            key={item.id}
+            id={item.id}
+            summary={item.summary}
+            details={item.details}
+          />
+        ))}
+      </GSContainer>
     </>
   );
 }
