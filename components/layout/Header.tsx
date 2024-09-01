04f407dae114a413757ec3b66792325058d2655c
aitzeng <aitzeng@gmail.com>
2024-09-01 04:44:47 -0700

    Added vertical padding to dropdown menus desktop

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -177,6 +177,7 @@ export default function Header({ isCalc }: HeaderProps) {
                         position: 'absolute',
                         backgroundColor: theme.palette.primary.dark,
                         width: '284px',
+                        py: '10px',
                         boxShadow: '0px 8px 16px 0px rgba(0,0,0,0.2)',
                         zIndex: 1,
                       }}
