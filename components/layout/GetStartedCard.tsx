179f387c3cdc5dc39542d5d6f8d1bc1b3894e0fe
Eleni Papanicolas <eleni.papanicolas@proton.me>
2024-09-03 19:04:47 -0400

    use markdown in card for step 3 substep cards

--- components/layout/GetStartedCard.tsx
+++ components/layout/GetStartedCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, Typography } from '@mui/material';
 import { Box } from '@mui/system';
+import MuiMarkdown from 'mui-markdown';
 import React from 'react';
 
 import { GetStartedStep } from '../../content/content.types.ts';
@@ -7,12 +8,12 @@ import theme from '../../styles/themes/theme.tsx';
 import { DarkButton } from '../helper/CustomButtons.tsx';
 
 export default function GetStartedCard({
-  title, overline, body, ctaText, ctaLink,
+  title, overline, body, ctaText, ctaLink, useMD, cardHeight,
 }: GetStartedStep) {
   return (
     <Card sx={{
       backgroundColor: theme.palette.secondary.light,
-      height: '404px',
+      height: cardHeight ? '444px' : '404px',
       width: '284px',
       padding: '32px 24px',
       mb: '32px',
@@ -45,10 +46,55 @@ export default function GetStartedCard({
               {title}
             </Typography>
           </Box>
-          <Typography variant="body2">
-            {body}
-          </Typography>
+          {useMD ? (
+            <Box>
+              <MuiMarkdown
+                overrides={{
+                  span: {
+                    component: Typography,
+                    props: {
+                      variant: 'body2',
+                    },
+                  },
+                  p: {
+                    component: Typography,
+                    props: {
+                      variant: 'body2',
+                      style: {
+                        marginBottom: '6px',
+                      },
+                    },
+                  },
+                  strong: {
+                    component: Typography,
+                    props: {
+                      variant: 'body2',
+                      sx: { fontWeight: '700', display: 'inline' },
+                    },
+                  },
+                  a: {
+                    component: 'a',
+                    props: {
+                      style: {
+                        color: theme.palette.link.main,
+                      },
+                    },
+                  },
+                }}
+              >
+                {body}
+              </MuiMarkdown>
+            </Box>
+          ) : (
+            <Typography variant="body2">
+              {body}
+            </Typography>
+          )}
         </Box>
+        {
+        ctaLink && ctaText
+
+        && (
         <Box sx={{
           height: '104px',
           padding: '32px 0',
@@ -59,6 +105,8 @@ export default function GetStartedCard({
             text={ctaText}
           />
         </Box>
+        )
+        }
       </CardContent>
     </Card>
   );
