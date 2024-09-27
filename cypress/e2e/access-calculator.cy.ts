2b39ad62fcaaeffbfd98442d52d77fae233107ef
Eleni Papanicolas <eleni.papanicolas@proton.me>
2024-09-27 14:36:29 +0300

    update access calc test with removal of head-start-2-cont

--- cypress/e2e/access-calculator.cy.ts
+++ cypress/e2e/access-calculator.cy.ts
@@ -4,7 +4,7 @@ describe('From Home Page', () => {
     // https://docs.cypress.io/guides/references/best-practices#Assigning-Return-Values
     cy.get('a:contains("Check eligibility"):visible')
       .as('accessCalculatorBtns')
-      .should('have.length', 2);
+      .should('have.length', 3);
     // Click on ACCESS CALCULATOR from Header
     cy.get('@accessCalculatorBtns').first().click();
     cy.url().should('include', '/calculator/head-initial-1-cont');
@@ -12,35 +12,35 @@ describe('From Home Page', () => {
     cy.get('[data-cy="calc-choice-0"]:visible')
       .as('initialCalculatorBtn')
       .should('have.length', 1)
-      .should('have.attr', 'href', '/calculator/head-start-2-cont');
+      .should('have.attr', 'href', '/calculator/head-start-3-cont');
     cy.get('@initialCalculatorBtn').click();
 
-    cy.url().should('include', '/calculator/head-start-2-cont');
+    cy.url().should('include', '/calculator/head-start-3-cont');
     cy.get('[data-cy="calc-choice-0"]:visible')
-      .should('have.attr', 'href', '/calculator/head-start-3-cont')
+      .should('have.attr', 'href', '/calculator/head-mis-4-cont')
       .click();
 
-    cy.url().should('include', '/calculator/head-start-3-cont');
+    cy.url().should('include', '/calculator/head-mis-4-cont');
   });
 
   it('its possible to access calculator from Hero btn', () => {
     cy.visit('/');
     cy.get('a:contains("Check eligibility"):visible').as('accessCalculatorBtns');
-    cy.get('@accessCalculatorBtns').should('have.length', 2);
+    cy.get('@accessCalculatorBtns').should('have.length', 3);
     cy.get('@accessCalculatorBtns').eq(1).click();
     cy.url().should('include', '/calculator/head-initial-1-cont');
 
     cy.get('[data-cy="calc-choice-0"]:visible')
       .as('initialCalculatorBtn')
       .should('have.length', 1)
-      .should('have.attr', 'href', '/calculator/head-start-2-cont');
+      .should('have.attr', 'href', '/calculator/head-start-3-cont');
     cy.get('@initialCalculatorBtn').click();
 
-    cy.url().should('include', '/calculator/head-start-2-cont');
+    cy.url().should('include', '/calculator/head-start-3-cont');
     cy.get('[data-cy="calc-choice-0"]:visible')
-      .should('have.attr', 'href', '/calculator/head-start-3-cont')
+      .should('have.attr', 'href', '/calculator/head-mis-4-cont')
       .click();
 
-    cy.url().should('include', '/calculator/head-start-3-cont');
+    cy.url().should('include', '/calculator/head-mis-4-cont');
   });
 });
