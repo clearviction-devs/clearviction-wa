10b6e69aeb429c4e2a68f5e6c34d51868628bbfd
Eleni Papanicolas <eleni.papanicolas@proton.me>
2024-10-20 19:49:18 +0300

    add favicon

--- components/IndividualPageHead.tsx
+++ components/IndividualPageHead.tsx
@@ -11,7 +11,7 @@ export default function IndividualPageHead({ title, metaContent }: Props) {
     <Head>
       <title>{title}</title>
       <meta name="description" content={metaContent} key="description" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.png" />
     </Head>
   );
 }
